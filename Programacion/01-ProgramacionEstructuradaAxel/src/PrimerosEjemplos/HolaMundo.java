012d9ad9e3670ddbaf89b681c829cdc7259acac7
Axel Tristan Segudo Magaña <90589832+Peeprs@users.noreply.github.com>
2025-02-27 00:05:12 -0600

    Delete Programacion directory

--- Programacion/01-ProgramacionEstructuradaAxel/src/PrimerosEjemplos/HolaMundo.java
+++
@@ -1,7 +0,0 @@
-package PrimerosEjemplos;
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
-}
-
--- Programacion/Programacion/01-ProgramacionEstructuradaAxel/src/PrimerosEjemplos/Hola_Mundo.java
+++
@@ -1,8 +0,0 @@
-
-import javax.swing.JOptionPane;
-
-public class Hola_Mundo {
-public static void main(String[] args) {
-    JOptionPane.showMessageDialog(null, "Hola");
-}
-}
