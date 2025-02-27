86f5b1b30b9f9e1eedc10e659a26b35eed6fac27
Axel Tristan Segudo Magaña <90589832+Peeprs@users.noreply.github.com>
2025-02-27 00:18:27 -0600

    Add files via upload

---
+++ 03-ProgramacionEstructurada/src/PrimerosCodigosNoCompletos/Acumuladores.java
@@ -0,0 +1,11 @@
+public class Acumuladores {
+    public static void main(String[] args) {
+        int total = 0;
+        total = total+ 3;
+        total = total+ 6;
+        total = total+ 4;
+
+        System.out.println(total);
+
+    }
+}
